3403f63f943b7cce7bb5b8ef1f62b6355a711995
Tank <johnson90.cm@gmail.com>
2021-11-10 23:49:54 -0700

    refactoring fs.read

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,41 +1,22 @@
 
 const router = require('express').Router();
-const { notes } = require('../../db/db.json');
+const fs = require('fs');
 
-function findById(id, notesArray) {
-    const result = notesArray.filter(notes => notes.id === id)[0];
-    return result;
-}
 
-router.get('/notes', (req, res) => {
-    let results = notes
-    res.json(results);
-    console.log(results);
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+
+    var notes = JSON.parse(data)
+
+    router.get('/notes', (req, res) => {
+        if (notes) {
+            res.json(notes);
+            console.log(notes)
+        } else {
+            res.send(404)
+        }
+    })
 });
 
-router.get('/notes/:id', (req, res) => {
-    const results = findById(req.params.id, notes);
-    if (results) {
-        res.json(results);
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-router.post('/notes', (req, res) => {
-    let results = notes
-    // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-    res.json(req.body);
-    results.push(req.body)
-});
-
-
-// function dbUpdate() {
-//     fs.writeFile("db/db.json",JSON.stringify(notes,'\t'), err => {
-//         if (err) throw err;
-//         return true;
-//     })
-// }
 
 module.exports = router;
